4bca47c30fa1f1c6bb7abc55d6fea55118a2fea8
zackducreux <zackducreux@gmail.com>
2024-07-16 10:55:07 +0200

    date

--- assets/js/currentdate.js
+++ assets/js/currentdate.js
@@ -1,6 +1,9 @@
 function getCurrentDate() {
-    const options = { year: 'numeric', month: 'long', day: 'numeric' };
-    return new Date().toLocaleDateString('en-US', options);
-  }
+  const date = new Date();
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-based
+  const year = date.getFullYear();
+  return `${day}/${month}/${year}`;
+}
 
   document.getElementById('currentDate').textContent = getCurrentDate();
