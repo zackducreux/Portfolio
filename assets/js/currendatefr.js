ae3444bdaf0d34657088793c85192591a1d8a9fb
zackducreux <zackducreux@gmail.com>
2024-07-16 10:59:33 +0200

    dd

--- assets/js/currendatefr.js
+++
@@ -1,6 +0,0 @@
-function getCurrentDatefr() {
-    const options = { year: 'numeric', month: 'long', day: 'numeric' };
-    return new Date().toLocaleDateString('fr-FR', options);
-  }
-
-  document.getElementById('currentDatefr').textContent = getCurrentDatefr();
